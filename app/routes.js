ab13dc927259192184edc86b05119c50e40cbe83
Corey R. Stagner <cresc0428@gmail.com>
2017-10-25 16:21:40 -0400

    Trying to interpolate multiple variables from express to pug

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,11 @@ console.log("||\u274c  Opened File [./app/routes.js]");
 console.log("\u26a0 == Finish Converting templage from EJS to Pug and have extend a Main.pug to keep code DRY");
 
 var Pantry = require("./models/pantry.js");
+var databaseUrl = "pantry";
+var collections = ["pantries"];
+var mongojs = require("mongojs");
 
+var db = mongojs(databaseUrl, collections);
 
 module.exports = function (app, passport) {
 
@@ -52,10 +56,30 @@ module.exports = function (app, passport) {
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/profile', isLoggedIn, function (req, res) {
-    // var stockItem = Pantry.find().where({"user": req.user});
-    // console.log("Items", stockItem);
+    var myPantry = [];
+    console.log("user", req.user.local.username);
+
+    db.pantries.find({ user: req.user.local.username}, function(err, data) {
+      // Log any errors if the server encounters one
+      if (err) {
+        console.log("error", err);
+      }
+      // Otherwise, send the result of this query to the browser
+      else {
+        // console.log("data", data);
+        for (var i = 0; i < data.length; i++) {
+          var eachItem = {
+            item: data[i].item,
+            quantity: data[i].quantity
+          };
+          myPantry.push(eachItem);
+        }
+      }
+      console.log("My pantry currently has: ", myPantry);
+    });
     res.render('profile.pug', {
-      user: req.user // get the user out of session and pass to template
+      "user": req.user,
+      "pantry": myPantry
     });// end res.render()
   });// end app.get('/profile')
 
