4cca7ea7e1152fb1353e3c7f9adb322a7a565cc3
MarCayce <caycecrosby@gmail.com>
2017-10-30 19:59:37 -0400

    recipe stuff

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 console.log("||\u2713  Opened File [./config/database.js]");
 
 module.exports = {
-  // url: "mongodb://localhost/pantry"
-  url: "mongodb://heroku_nrb33b3m:o0fuphn0prlv0tl0jqs7qag9eq@ds161304.mlab.com:61304/heroku_nrb33b3m"
+  url: "mongodb://localhost/pantry"
+  // url: "mongodb://heroku_nrb33b3m:o0fuphn0prlv0tl0jqs7qag9eq@ds161304.mlab.com:61304/heroku_nrb33b3m"
   // key: "DW32hiLr5lnUot3YXB8hx4qgVGaEQVS0"
 };// end module.exports
 
 // Local DB link
+
+// for each ingredient push 
