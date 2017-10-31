e0c3c2992eb4826498c0fdead3102bca0343dc59
Corey R. Stagner <cresc0428@gmail.com>
2017-10-30 20:00:50 -0400

    build recipe Schema

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 console.log("||\u2713  Opened File [./config/database.js]");
 
 module.exports = {
+
+  // Local Database for Dev
   url: "mongodb://localhost/pantry"
+
+  // MLAB Database for Prod
   // url: "mongodb://heroku_nrb33b3m:o0fuphn0prlv0tl0jqs7qag9eq@ds161304.mlab.com:61304/heroku_nrb33b3m"
   // key: "DW32hiLr5lnUot3YXB8hx4qgVGaEQVS0"
-};// end module.exports
 
-// Local DB link
+};// end module.exports
