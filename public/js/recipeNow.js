c0397a992d22cff6a7d35958a86e22efa40123e5
Corey R. Stagner <cresc0428@gmail.com>
2017-11-09 21:31:09 -0500

    did this work?

--- public/js/recipeNow.js
+++ public/js/recipeNow.js
@@ -18,7 +18,7 @@ function displayRecipes(recipe) {
     recipe.forEach(function (item, idx) {
         console.log("item:", item);
         console.log("idx:", idx);
-        $("#recipeNowDiv").append("<div class='recipeDiv'><img src="+ item.image_url + " alt=" + item.title + "><div>" + item.title + "</div>");
+        $("#recipeNowDiv").append("<a href='/recipe/" + item._id + "'><div class='recipeDiv'><img src="+ item.image_url + " alt=" + item.title + "><div>" + item.title + "</div>");
     });
 }
 
